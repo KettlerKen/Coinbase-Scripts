22daab655cd5da62e14c8667bade6664f4d5e676
Kenny Kettler <33336996+KettlerKen@users.noreply.github.com>
2021-01-15 20:42:41 -0800

    Ad venv and ps script

--- Scripts/test.py
+++ Scripts/test.py
@@ -8,7 +8,7 @@ import os
 import re
 #from slackclient import SlackClient
 
-MESSAGE_TIMEOUT = 600
+MESSAGE_TIMEOUT = 60
 QUERY_TIMEOUT = 10
 public_client = cbpro.PublicClient()
 
@@ -16,11 +16,11 @@ coin_tickers = ["ETH-USD", "BTC-USD", "XLM-USD", "GRT-USD", "ATOM-USD"]
 
 float_keys    = ['last', 'daily_change', 'open', 'high', 'low', 'volume']
 
-formatted_table_rows = ['open', 'last']
+formatted_table_rows = ['last', 'open', 'daily_change_percent', 'daily_change_total']
 
 
 def slack_message(message):
-    wekbook_url = 'https://hooks.slack.com/services/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'
+    wekbook_url = 'https://hooks.slack.com/services/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'
 
     data = {
         'text': message,
@@ -49,52 +49,52 @@ def test_change(data):
 
 
 def main():
-    rows, columns = os.popen('stty size', 'r').read().split()
-    rows = int(rows)
-    columns = int(columns)
-
-
     while True:
-        formatted_table = ()
-        formatted_table = beautifultable.BeautifulTable()
-        formatted_table.columns.header = ['ticker'] + formatted_table_rows
-        rows = []
+        try:
+            formatted_table = ()
+            formatted_table = beautifultable.BeautifulTable()
+            formatted_table.columns.header = ['ticker'] + formatted_table_rows
+            rows = []
 
-        data_obj = []
+            data_obj = []
 
-        for ticker in coin_tickers:
-            data = public_client.get_product_24hr_stats(ticker)
-            row = []
-            
-            for key, value in data.items():
-                if key in float_keys:
-                    data[key] = float(data[key])
-
-            data['daily_change'] = round((data['last'] - data['open']), 2)
-
-            #print("======================== {} ========================".format(ticker))
-            # for key, value in data.items():
-            #     just_key = key.rjust(15, " ")
-                # if key in keys_to_print:
-                    #print("{}: ${}".format(just_key, value))
-
-            row.append(ticker)
-            for key in formatted_table_rows:
-                row.append(data[key])
-
-            rows.append(row)
-            data_obj.append(data)
+            for ticker in coin_tickers:
+                data = public_client.get_product_24hr_stats(ticker)
+                row = []
                 
-        for row in rows:
-            formatted_table.append_row(row)   
+                for key, value in data.items():
+                    if key in float_keys:
+                        data[key] = float(data[key])
 
-        print(formatted_table)
+                data['daily_change_total'] = round((data['last'] - data['open']), 2)
+                data['daily_change_percent'] = str(round(((data['last'] - data['open'])/data['open'])*100, 2)) + '%'
+                
+                #print("======================== {} ========================".format(ticker))
+                # for key, value in data.items():
+                #     just_key = key.rjust(15, " ")
+                    # if key in keys_to_print:
+                        #print("{}: ${}".format(just_key, value))
 
-        if test_change(data_obj):
-            slack_message("```"+str(formatted_table)+"```")
-            time.sleep(MESSAGE_TIMEOUT)
+                row.append(ticker)
+                for key in formatted_table_rows:
+                    row.append(data[key])
 
-        time.sleep(QUERY_TIMEOUT)
+                rows.append(row)
+                data_obj.append(data)
+                    
+            for row in rows:
+                formatted_table.append_row(row)   
+
+            print(formatted_table)
+
+            if test_change(data_obj):
+                slack_message("```"+str(formatted_table)+"```")
+                time.sleep(MESSAGE_TIMEOUT)
+
+            time.sleep(QUERY_TIMEOUT)
+        except:
+            print("OOPS IM BROKE")
+            time.sleep(QUERY_TIMEOUT)
 
 
 if __name__ == "__main__":
